6c90687cc7919e49fb9fa88e432e6c97850e285c
Michael Taylor <darday1984@gmail.com>
2020-07-07 19:46:06 +0100

    update

--- se_documentation_ui/src/main/resources/static/js/archiveManageMent.js
+++ se_documentation_ui/src/main/resources/static/js/archiveManageMent.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
+const DEFAULT_LIMIT= 10;
+const DEFAULT_OFFSET = 0;
 	
-	Utils.ajax(URLBASE+"/archivedDocumentsManagement/getArchivedEntries", {"limit": 4,"offset":2}, (response)=>{
-			Utils.log(response);
-	});
+$(document).ready(function(){
+	getArchiveEntries();	
 });
+
+getArchiveEntries = (limit, offset) => {
+	if(limit == undefined || offset == undefined){
+		limit = DEFAULT_LIMIT;
+		offset == DEFAULT_OFFSET;
+	}
+	
+	Utils.ajax(URLBASE+"/archivedDocumentsManagement/getArchivedEntries", {"limit": limit,"offset":offset}, (response)=>{
+		Utils.log(response);
+});
+}
